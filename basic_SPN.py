fb2561423f9ab222c3ccff1d2eaf43adb4fb489f
Kris Hicks <c.hicks@cs.bham.ac.uk>
2016-12-02 12:11:03 +0000

    First commit

---
+++ basic_SPN.py
@@ -0,0 +1,10 @@
+#A basic Substitution-Permutation Network cipher, implemented by following 
+#'A Tutorial on Linear and Differential Cryptanalysis'
+# by Howard M. Heys
+#
+# 02/12.16 Alice Hicks 
+#
+#Basic SPN cipher which takes as input a 16-bit input block and has 4 rounds.
+#Each round consists of (1) substitution (2) transposition (3) key mixing
+
+
